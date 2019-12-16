1ee43bdbd203522d7e6bf27ce35f32c9b9bf5ecb
Nam Vuong <namnv04@gmail.com>
2019-12-15 20:40:00 -0500

    Proxy with basic auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const proxy = require('http-proxy-middleware');
+const proxy = require('express-http-proxy');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', proxy({ target: 'http://postman-echo.com', changeOrigin: true }));
+// Cors handler
+app.use(cors({
+  credentials: true
+}));
+
+app.use('/path-to-proxy', proxy({
+  preserveHostHrd: true,
+  proxyReqPathResolver(req) {
+    return `/path-${req.url}`;
+  },
+  proxyReqOtpDecorator(proxyReqOtp, srcReq) {
+    return new Promise((resolve, reject) => {
+      const auth = `Basic ${Buffer.from('username:password').toString('base64')}`;
+      proxyReqOtp.headers['Authorization'] = auth; // Optionals if baisc auth is needed
+      resolve(proxyReqOtp);
+    })
+  }
+}));
 
 app.listen(3000, () => console.log('App started at 3000...'));
